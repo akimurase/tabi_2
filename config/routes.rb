cdcbe8bed2f12aa8543bb2d39d13c69a729c019e
akimurase <mosworldfor@gmail.com>
2020-11-04 15:16:57 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  # get 'events/index'
-  # get 'events/new'
-  # get 'events/create'
-  # get 'events/shwo'
-  # get 'events/edit'
-  # get 'events/update'
-  # get 'events/destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'events#index'
-  resources :events
-
+  resources :events do
+    collection do
+      post :confirm
+    end
+  end
 end
