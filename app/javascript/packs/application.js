e14d780689839a0d0d504ccc7ea8e9b1938dd1ce
akimurase <mosworldfor@gmail.com>
2020-11-09 13:29:52 +0900

    Jquery導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../card") 
+require("../test") 
+require('jquery')
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
